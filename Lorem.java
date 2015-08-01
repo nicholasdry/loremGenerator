1f987be1942efa94514e860f0da6cc865eb923d5
Paul Vorobyev <paul.vorobyev@gmail.com>
2015-07-31 23:57:31 -0400

    made program more modular + removed superfluous code

--- Lorem.java
+++ Lorem.java
@@ -6,36 +6,29 @@ public class Lorem {
 
 		Scanner input = new Scanner(System.in);
 
-		String [] lorem = {"Lorem", "Ipsum", "Donec", "Sed", "odio", "dui", "Vivamus", "sagittis", "lacus", "vel", "augue", "laoreet", "rutrum", 
-							  "faucibus", "facilisis", "Maecenas", "mattis", "magna", "diam", "eget", "risus", "varius", "blandit", "sit", "amet"};
 		
 		System.out.print("How many words do you want? ");
 		int count = input.nextInt();
 		System.out.println();
 
-		long startTime = System.currentTimeMillis();
 
 		for (int i=0;i<count;i++) {
-			int place = genNum(count);
-			System.out.print(lorem[place] + " ");
+			System.out.print(genWord() + " ");
 		}
 
 		System.out.println();
-
-		long endTime = System.currentTimeMillis();
-
-		long totalTime = endTime - startTime;
-
-		double totalSeconds = totalTime*.001;
-
-		System.out.printf("Time to Generate: %1.2f", totalSeconds);
-		System.out.println();
-
 	}
 
-	public static int genNum(int count) {
+    public static String genWord(){
+		String [] lorem = {"Lorem", "Ipsum", "Donec", "Sed", "odio", "dui", "Vivamus", "sagittis", "lacus", "vel", "augue", "laoreet", "rutrum", 
+							  "faucibus", "facilisis", "Maecenas", "mattis", "magna", "diam", "eget", "risus", "varius", "blandit", "sit", "amet"};
 
-		double num = (int)(Math.floor(Math.random()*20)+1);
+        return lorem[genNum(lorem.length)];
+    }
+
+	public static int genNum(int numElements) {
+
+		double num = (int)(Math.floor(Math.random()*numElements)+1);
 
 		return (int)num;
 	}
